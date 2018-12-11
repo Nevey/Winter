ae846e65455654342bbc180465aba2a59485f850
Nils <its.just.nils@gmail.com>
2018-12-11 22:52:54 +0100

    fixed up rotation on movement and clamp to max movement speed

--- Assets/[Game]/Scripts/Gameplay/Translation/MovementController.cs
+++ Assets/[Game]/Scripts/Gameplay/Translation/MovementController.cs
@@ -1,5 +1,7 @@
+using System;
 using Game.Gameplay.Camera;
 using Game.Gameplay.Controls.Services;
+using Game.Utilities;
 using UnityEngine;
 
 namespace Game.Gameplay.Translation
@@ -10,12 +12,15 @@ namespace Game.Gameplay.Translation
         [SerializeField] private ThirdPersonCamera thirdPersonCamera;
 
         [Header("Movement Settings")]
-        [SerializeField] private float movementSpeed = 1f;
+        [SerializeField] private float horizontalMovementSpeed = 0.5f;
+        [SerializeField] private float verticalMovementSpeed = 1f;
         [SerializeField] private float movementSmoothWeight = 0.2f;
 
         [Header("Rotation Settings")]
         [SerializeField] private float rotationSmoothWeight = 0.1f;
         [SerializeField] private float maxRotationSpeed = 500f;
+
+        private Vector2 inputDirection = Vector2.zero;
 
         // Horizontal movement fields
         private Vector3 horizontalVector;
@@ -43,21 +48,24 @@ namespace Game.Gameplay.Translation
             ControlsService.Instance.VerticalInputEvent -= OnVerticalInput;
         }
 
-        private void OnHorizontalInput(float obj)
+        private void OnHorizontalInput(float inputHorizontal)
         {
-            targetHorizontalVector =
-                thirdPersonCamera.transform.right * obj * movementSpeed * Time.deltaTime;
+            targetHorizontalVector = thirdPersonCamera.transform.right * inputHorizontal
+                                                                       * horizontalMovementSpeed
+                                                                       * Time.deltaTime;
+
+            inputDirection.x = inputHorizontal;
         }
 
-        private void OnVerticalInput(float inputDelta)
+        private void OnVerticalInput(float inputVertical)
         {
-            targetVerticalVector =
-                thirdPersonCamera.transform.forward * inputDelta * movementSpeed * Time.deltaTime;
+            targetVerticalVector = thirdPersonCamera.transform.forward * inputVertical
+                                                                       * verticalMovementSpeed
+                                                                       * Time.deltaTime;
 
             targetVerticalVector.y = 0f;
 
-            targetVerticalEuler =
-                inputDelta > 0f ? thirdPersonCamera.transform.eulerAngles : Vector3.zero;
+            inputDirection.y = inputVertical;
 
             UpdateMovement();
             UpdateRotation();
@@ -71,18 +79,17 @@ namespace Game.Gameplay.Translation
             horizontalVector = Vector3.SmoothDamp(horizontalVector, targetHorizontalVector,
                 ref horizontalVectorVelocity, movementSmoothWeight);
 
-            transform.position += verticalVector + horizontalVector;
+            Vector3 positionStep = horizontalVector + verticalVector;
+            positionStep = Vector3.ClampMagnitude(positionStep, verticalMovementSpeed * Time.deltaTime);
 
-            // TODO: Clamp movement magnitude
+            transform.position += positionStep;
         }
 
         private void UpdateRotation()
         {
-            Vector3 targetRotation = targetVerticalEuler;
-
-            Quaternion desiredRotation = targetRotation == Vector3.zero
+            Quaternion desiredRotation = Math.Abs(inputDirection.magnitude) < 0.01f
                 ? transform.rotation
-                : Quaternion.Euler(targetRotation);
+                : thirdPersonCamera.transform.rotation;
 
             float angle = Mathf.SmoothDampAngle(transform.rotation.eulerAngles.y,
                 desiredRotation.eulerAngles.y, ref rotationVelocity, rotationSmoothWeight,
