afbabf3efef58d482d55bff31f640ecd34c48a46
Nils <its.just.nils@gmail.com>
2019-01-03 14:47:49 +0100

    add snow shader, snow playground is test area

---
+++ Assets/[Game]/Scripts/Snow/DrawWithMouse.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class DrawWithMouse : MonoBehaviour
+{
+    public Camera camera;
+
+    public Shader drawShader;
+
+    private RenderTexture splatMap;
+
+    private Material snowMaterial, drawMaterial;
+    
+    private RaycastHit hit;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        drawMaterial = new Material(drawShader);
+        drawMaterial.SetVector("_Color", Color.red);
+
+        snowMaterial = GetComponent<MeshRenderer>().material;
+        splatMap = new RenderTexture(1024, 1024, 0, RenderTextureFormat.ARGBFloat);
+        snowMaterial.SetTexture("_Splat", splatMap);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                drawMaterial.SetVector("_Coordinate", new Vector4(hit.textureCoord.x, hit.textureCoord.y, 0f, 0f));
+                
+                RenderTexture temp = RenderTexture.GetTemporary(splatMap.width, splatMap.height, 0,
+                    RenderTextureFormat.ARGBFloat);
+                
+                Graphics.Blit(splatMap, temp);
+                Graphics.Blit(temp, splatMap, drawMaterial);
+                
+                RenderTexture.ReleaseTemporary(temp);
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, 256, 256), splatMap, ScaleMode.ScaleToFit, false, 1);
+    }
+}
