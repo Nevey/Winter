d8de514059a38c2251177846b105aec3cb22a4fe
Nils <its.just.nils@gmail.com>
2019-01-20 23:57:32 +0100

    fix loading scenes in builds

---
+++ Assets/[Game]/Scripts/Scenes/RequiredScenes.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace Game.Scenes
+{
+    public class RequiredScenes : MonoBehaviour
+    {
+
+#if !UNITY_EDITOR
+        private void Awake()
+        {
+            SceneController.LoadSceneAdditive("ConsoleScene");
+            SceneController.LoadSceneAdditive("SurfacePainterPlayground");
+        }
+#endif
+    }
+}
